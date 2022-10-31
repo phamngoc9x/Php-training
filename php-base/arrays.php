0369479fc5218af09fef62e1645ca060cfb95616
pham.van.ngoc <pham.van.ngoc@suns-Mac-mini-2.local>
2022-10-31 17:28:56 +0700

    laravel

--- php-base/arrays.php
+++ php-base/arrays.php
@@ -1,8 +1,38 @@
 <?php 
   echo 'Array examples'."<br>";
 
-  $some_number = [1,2,3,4];
-  $fruits = ['pineapple', 'melon', 'apple'];
-  print_r($fruits);
-  echo $fruits[1];
+  // $some_number = [1,2,3,4];
+  // $fruits = ['pineapple', 'melon', 'apple'];
+  // print_r($fruits);
+  // echo $fruits[1];
+
+  // $color = [
+  //   3 => 'red',
+  //   5 => 'green',
+  //   7 => 'blue',
+  // ];
+
+  // echo $color[7]
+
+  $person = [
+    [
+      'full_name' => 'Pham Van Ngoc',
+      'age' => 41,
+      'email' => 'ngoc@gmail.com',
+    ],
+    [
+      'full_name' => 'Raccoon',
+      'age' => 23,
+      'email' => 'raccon@gmail.com',
+    ],
+    [
+      'full_name' => 'John',
+      'age' => 33,
+      'email' => 'John@gmail.com',
+    ],
+  ];
+
+  print_r($person);
+  echo $person[2]['full_name']
+
 ?>
